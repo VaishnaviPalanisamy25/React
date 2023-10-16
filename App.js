c06bf33eda7dd3f9fe8e7ef2261e62579345a34e
VaishanviPalanisamy25 <vaishu25082000@gmail.com>
2023-10-16 16:09:04 +0530

    episode -1

---
+++ App.js
@@ -0,0 +1,15 @@
+
+const parent = React.createElement("div",{id:'parent'},
+[React.createElement("div",{id:"child1"},[
+    React.createElement("h1",{id:"child1"},"Inside child1"),
+    React.createElement("h2",{id:"sibling"},"Inside sibling")]),
+React.createElement("div",{id:"child2"},[
+    React.createElement("h1",{id:"child2"},"Inside child1"),
+    React.createElement("h2",{id:"sibling"},"Inside sibling")
+])]
+);
+
+console.log(parent);
+const root= ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
